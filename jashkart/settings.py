bcece4682358695bdcf987b003af9ee37c9ee7ab
Jash <shahjash2809@gmail.com>
2023-05-22 16:10:05 +0530

    changes required

--- jashkart/settings.py
+++ jashkart/settings.py
@@ -149,5 +149,6 @@ MESSAGE_TAGS = {
 # EMAIL_USE_TLS = True
 import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS =  os.path.join(BASE_DIR, 'static'),
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
